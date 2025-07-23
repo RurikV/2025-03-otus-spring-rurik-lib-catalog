2f115c1058edb6c9db92d327ffe88a3a661420de
rurik <rurik@stv.ee>
2025-07-23 17:30:56 +0300

    Add `Comment` migration step to batch configuration: reader, processor, writer integration. Rename `migrationStep` to `bookMigrationStep`. Update job flow to include `commentMigrationStep`.

--- hw14-batch/src/main/java/ru/otus/hw/batch/processors/CommentItemProcessor.java
+++ hw14-batch/src/main/java/ru/otus/hw/batch/processors/CommentItemProcessor.java
@@ -2,6 +2,7 @@ package ru.otus.hw.batch.processors;
 
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.mappers.EntityMapper;
 import ru.otus.hw.models.jpa.Comment;
@@ -18,11 +19,7 @@ public class CommentItemProcessor implements ItemProcessor<MongoComment, Comment
     }
     
     @Override
-    public Comment process(MongoComment mongoComment) throws Exception {
-        if (mongoComment == null) {
-            return null;
-        }
-        
+    public Comment process(@NonNull MongoComment mongoComment) {
         // Transform MongoDB comment to JPA comment
         Comment comment = entityMapper.mapToComment(mongoComment);
         
--- hw14-batch/src/main/java/ru/otus/hw/config/BatchConfiguration.java
+++ hw14-batch/src/main/java/ru/otus/hw/config/BatchConfiguration.java
@@ -10,10 +10,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 import ru.otus.hw.batch.processors.BookItemProcessor;
+import ru.otus.hw.batch.processors.CommentItemProcessor;
 import ru.otus.hw.batch.readers.MongoBookItemReader;
+import ru.otus.hw.batch.readers.MongoCommentItemReader;
 import ru.otus.hw.batch.writers.BookItemWriter;
+import ru.otus.hw.batch.writers.CommentItemWriter;
 import ru.otus.hw.models.jpa.Book;
+import ru.otus.hw.models.jpa.Comment;
 import ru.otus.hw.models.mongo.MongoBook;
+import ru.otus.hw.models.mongo.MongoComment;
 
 @Configuration
 public class BatchConfiguration {
@@ -27,23 +32,35 @@ public class BatchConfiguration {
     private final BookItemProcessor bookItemProcessor;
 
     private final BookItemWriter bookItemWriter;
+
+    private final MongoCommentItemReader mongoCommentItemReader;
+
+    private final CommentItemProcessor commentItemProcessor;
+
+    private final CommentItemWriter commentItemWriter;
     
     @Autowired
     public BatchConfiguration(JobRepository jobRepository,
                              PlatformTransactionManager transactionManager,
                              MongoBookItemReader mongoBookItemReader,
                              BookItemProcessor bookItemProcessor,
-                             BookItemWriter bookItemWriter) {
+                             BookItemWriter bookItemWriter,
+                             MongoCommentItemReader mongoCommentItemReader,
+                             CommentItemProcessor commentItemProcessor,
+                             CommentItemWriter commentItemWriter) {
         this.jobRepository = jobRepository;
         this.transactionManager = transactionManager;
         this.mongoBookItemReader = mongoBookItemReader;
         this.bookItemProcessor = bookItemProcessor;
         this.bookItemWriter = bookItemWriter;
+        this.mongoCommentItemReader = mongoCommentItemReader;
+        this.commentItemProcessor = commentItemProcessor;
+        this.commentItemWriter = commentItemWriter;
     }
     
     @Bean
-    public Step migrationStep() {
-        return new StepBuilder("migrationStep", jobRepository)
+    public Step bookMigrationStep() {
+        return new StepBuilder("bookMigrationStep", jobRepository)
                 .<MongoBook, Book>chunk(10, transactionManager)
                 .reader(mongoBookItemReader)
                 .processor(bookItemProcessor)
@@ -52,9 +69,20 @@ public class BatchConfiguration {
     }
     
     @Bean
+    public Step commentMigrationStep() {
+        return new StepBuilder("commentMigrationStep", jobRepository)
+                .<MongoComment, Comment>chunk(10, transactionManager)
+                .reader(mongoCommentItemReader)
+                .processor(commentItemProcessor)
+                .writer(commentItemWriter)
+                .build();
+    }
+    
+    @Bean
     public Job migrationJob() {
         return new JobBuilder("migrationJob", jobRepository)
-                .start(migrationStep())
+                .start(bookMigrationStep())
+                .next(commentMigrationStep())
                 .build();
     }
 }
