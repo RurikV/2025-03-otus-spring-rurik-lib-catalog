14be042b4700637b76203b96f3713a6da1a59e40
rurik <rurik@stv.ee>
2025-07-27 02:52:35 +0300

    Import `ConditionalMongoTestConfig` in `SecurityIntegrationTest` to support conditional MongoDB configuration in security-related tests.

--- hw12-mvc-auth/src/test/java/ru/otus/hw/controllers/SecurityIntegrationTest.java
+++ hw12-mvc-auth/src/test/java/ru/otus/hw/controllers/SecurityIntegrationTest.java
@@ -3,11 +3,13 @@ package ru.otus.hw.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import ru.otus.hw.config.ConditionalMongoTestConfig;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -17,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @WebAppConfiguration
+@Import(ConditionalMongoTestConfig.class)
 class SecurityIntegrationTest {
 
     @Autowired
