b77fc5388bbc02748e950f229c6d0b7e7a8391e6
rurik <rurik@stv.ee>
2025-07-27 03:16:40 +0300

    Replace `ConditionalMongoTestConfig` with `TestMongoConfig` in `SecurityIntegrationTest` for improved test configuration consistency.

--- hw12-mvc-auth/src/test/java/ru/otus/hw/controllers/SecurityIntegrationTest.java
+++ hw12-mvc-auth/src/test/java/ru/otus/hw/controllers/SecurityIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import ru.otus.hw.config.ConditionalMongoTestConfig;
+import ru.otus.hw.config.TestMongoConfig;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @WebAppConfiguration
-@Import(ConditionalMongoTestConfig.class)
+@Import(TestMongoConfig.class)
 class SecurityIntegrationTest {
 
     @Autowired
