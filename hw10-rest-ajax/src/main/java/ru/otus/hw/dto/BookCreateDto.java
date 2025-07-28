0c8fcb68b5462d81ece5b1a796ea5d6ef986f869
rurik <rurik@stv.ee>
2025-07-28 14:45:59 +0300

    Add validation annotations to `BookCreateDto` for title and author fields to enforce non-blank constraints.

--- hw10-rest-ajax/src/main/java/ru/otus/hw/dto/BookCreateDto.java
+++ hw10-rest-ajax/src/main/java/ru/otus/hw/dto/BookCreateDto.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import jakarta.validation.constraints.NotBlank;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookCreateDto {
+    @NotBlank(message = "Title is required and cannot be empty")
     private String title;
     
+    @NotBlank(message = "Author is required")
     private String authorId;
     
     private Set<String> genreIds;
