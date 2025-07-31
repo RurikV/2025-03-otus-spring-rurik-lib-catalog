81db1b4a0eb3ea619d77d1aeeb6d559839f00eb7
rurik <rurik@stv.ee>
2025-07-31 19:17:33 +0300

    Remove R2dbcConfig and simplify `listAuthors` method in `AuthorHandler` by eliminating unused debug logs and data transformations.

--- hw11-webflux/src/main/java/ru/otus/hw/handlers/AuthorHandler.java
+++ hw11-webflux/src/main/java/ru/otus/hw/handlers/AuthorHandler.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-import ru.otus.hw.dto.AuthorDto;
 import ru.otus.hw.services.AuthorService;
 
 @Slf4j
@@ -20,26 +19,8 @@ public class AuthorHandler {
     public Mono<ServerResponse> listAuthors(@SuppressWarnings("unused") ServerRequest request) {
         log.info("[DEBUG_LOG] AuthorHandler.listAuthors() called");
         
-        return authorService.findAll()
-                .doOnNext(author -> log.info("[DEBUG_LOG] Found author: {}", author.getFullName()))
-                .collectList()
-                .doOnSuccess(authors -> log.info("[DEBUG_LOG] Retrieved {} authors from service: {}", 
-                        authors.size(), authors.stream().map(AuthorDto::getFullName).toList()))
-                .doOnError(error -> log.error("[DEBUG_LOG] Error retrieving authors", error))
-                .flatMap(authors -> {
-                    log.info("[DEBUG_LOG] Rendering author/list template with {} authors", authors.size());
-                    log.info("[DEBUG_LOG] About to render template with model key 'authors' using Map approach");
-                    
-                    // Use Map-based approach for better Thymeleaf compatibility
-                    java.util.Map<String, Object> model = new java.util.HashMap<>();
-                    model.put("authors", authors);
-                    log.info("[DEBUG_LOG] Model map created with authors key, map size: {}", model.size());
-                    
-                    return ServerResponse.ok()
-                            .contentType(MediaType.TEXT_HTML)
-                            .render("author/list", model);
-                })
-                .doOnSuccess(response -> log.info("[DEBUG_LOG] Successfully rendered authors page"))
-                .doOnError(error -> log.error("[DEBUG_LOG] Error rendering authors page", error));
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .render("author/list");
     }
 }
