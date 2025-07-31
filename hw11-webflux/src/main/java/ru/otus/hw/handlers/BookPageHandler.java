fde6e80adf18cb4fd3e17c7f334dd8bc4cc75a9c
rurik <rurik@stv.ee>
2025-07-31 19:51:36 +0300

    Remove unused services, DTOs, and related logic from `BookPageHandler` to simplify code and streamline functionality.

--- hw11-webflux/src/main/java/ru/otus/hw/handlers/BookPageHandler.java
+++ hw11-webflux/src/main/java/ru/otus/hw/handlers/BookPageHandler.java
@@ -7,18 +7,13 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import ru.otus.hw.dto.BookCreateDto;
-import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.BookFormDto;
 import ru.otus.hw.dto.BookUpdateDto;
 import ru.otus.hw.exceptions.EntityNotFoundException;
-import ru.otus.hw.services.AuthorService;
 import ru.otus.hw.services.BookService;
-import ru.otus.hw.services.CommentService;
-import ru.otus.hw.services.GenreService;
 
 import java.net.URI;
 import java.util.HashSet;
-import java.util.Map;
 
 import static ru.otus.hw.handlers.ErrorHandlingUtils.handlePageErrors;
 
@@ -26,13 +21,7 @@ import static ru.otus.hw.handlers.ErrorHandlingUtils.handlePageErrors;
 @RequiredArgsConstructor
 public class BookPageHandler {
 
-    private final AuthorService authorService;
-
-    private final GenreService genreService;
-
     private final BookService bookService;
-
-    private final CommentService commentService;
 
     public Mono<ServerResponse> listBooks(ServerRequest request) {
         return ServerResponse.ok()
@@ -41,54 +30,30 @@ public class BookPageHandler {
     }
 
     public Mono<ServerResponse> viewBook(ServerRequest request) {
-        String id = request.pathVariable("id");
-        return Mono.zip(bookService.findById(id), commentService.findByBookId(id).collectList())
-                .flatMap(tuple -> ServerResponse.ok()
-                        .contentType(MediaType.TEXT_HTML)
-                        .render("book/view", Map.of(
-                                "bookId", id,
-                                "book", tuple.getT1(),
-                                "comments", tuple.getT2()
-                        )))
-                .switchIfEmpty(ServerResponse.notFound().build())
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .render("book/view")
                 .onErrorResume(handlePageErrors("/"));
     }
 
     public Mono<ServerResponse> newBookForm(ServerRequest request) {
-        return Mono.zip(authorService.findAll().collectList(), genreService.findAll().collectList())
-                .flatMap(tuple -> ServerResponse.ok()
-                        .contentType(MediaType.TEXT_HTML)
-                        .render("book/form", Map.of(
-                                "book", new BookDto(),
-                                "authors", tuple.getT1(),
-                                "genres", tuple.getT2()
-                        )))
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .render("book/form")
                 .onErrorResume(handlePageErrors("/"));
     }
 
     public Mono<ServerResponse> editBookForm(ServerRequest request) {
-        String id = request.pathVariable("id");
-        return Mono.zip(
-                bookService.findById(id),
-                authorService.findAll().collectList(),
-                genreService.findAll().collectList()
-        ).flatMap(tuple -> ServerResponse.ok()
+        return ServerResponse.ok()
                 .contentType(MediaType.TEXT_HTML)
-                .render("book/form", Map.of(
-                        "bookId", id,
-                        "book", tuple.getT1(),
-                        "authors", tuple.getT2(),
-                        "genres", tuple.getT3()
-                )))
-                .switchIfEmpty(ServerResponse.notFound().build())
+                .render("book/form")
                 .onErrorResume(handlePageErrors("/"));
     }
 
     public Mono<ServerResponse> deleteBookConfirm(ServerRequest request) {
-        String id = request.pathVariable("id");
         return ServerResponse.ok()
                 .contentType(MediaType.TEXT_HTML)
-                .render("book/delete", Map.of("bookId", id));
+                .render("book/delete");
     }
 
     public Mono<ServerResponse> createBook(ServerRequest request) {
@@ -130,33 +95,9 @@ public class BookPageHandler {
     }
 
     private Mono<ServerResponse> handleBookFormError(String errorMessage, BookFormDto formDto, String bookId) {
-        var bookDto = new BookDto(bookId, formDto.getTitle(), null, null);
-        
-        return Mono.zip(authorService.findAll().collectList(), genreService.findAll().collectList())
-                .flatMap(tuple -> {
-                    var model = createErrorModel(errorMessage, bookDto, bookId, tuple.getT1(), tuple.getT2());
-                    return ServerResponse.ok()
-                            .contentType(MediaType.TEXT_HTML)
-                            .render("book/form", model);
-                });
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .render("book/form");
     }
 
-    private Map<String, Object> createErrorModel(String errorMessage, BookDto bookDto, String bookId, 
-                                                Object authors, Object genres) {
-        if (bookId != null) {
-            return Map.of(
-                    "error", errorMessage,
-                    "bookId", bookId,
-                    "book", bookDto,
-                    "authors", authors,
-                    "genres", genres
-            );
-        }
-        return Map.of(
-                "error", errorMessage,
-                "book", bookDto,
-                "authors", authors,
-                "genres", genres
-        );
-    }
 }
